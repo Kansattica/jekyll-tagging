e728ebb33b7ca81eff18c2a2b7b8175df9fb158c
Grace H <grace@hypnovir.us>
2020-06-16 14:48:12 -0700

    See if this makes the SEO tag happy.

--- lib/jekyll/tagging.rb
+++ lib/jekyll/tagging.rb
@@ -49,10 +49,8 @@ module Jekyll
       self.class.types.each { |type|
         if layout = site.config["tag_#{type}_layout"]
 		  last_modified = posts.max_by { |e| e.data['last_modified_at'].last_modified_at_time };
-          data = { 'layout' => layout, 'posts' => posts.sort.reverse!, 'tag' => tag, 'last_modified_at' => last_modified.data['last_modified_at'], 'date_modified' => last_modified.data['last_modified_at'].last_modified_at_time }
+		  data = { 'layout' => layout, 'posts' => posts.sort.reverse!, 'tag' => tag, 'last_modified_at' => last_modified.data['last_modified_at'], 'seo' => {'date_modified' => last_modified.data['last_modified_at'].last_modified_at_time} }
           data.merge!(site.config["tag_#{type}_data"] || {})
-
-		  pp data
 
           name = yield data if block_given?
           name ||= tag
