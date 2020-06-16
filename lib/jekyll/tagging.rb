15b13e571c09f93e0453bbb3ab8cd1dd4d77c34b
Grace H <grace@hypnovir.us>
2020-06-16 14:53:29 -0700

    Once more into the breach.

--- lib/jekyll/tagging.rb
+++ lib/jekyll/tagging.rb
@@ -1,6 +1,7 @@
 require 'nuggets/range/quantile'
 require 'nuggets/i18n'
 require 'erb'
+require 'pry'
 
 module Jekyll
 
@@ -48,6 +49,7 @@ module Jekyll
     def new_tag(tag, posts)
       self.class.types.each { |type|
         if layout = site.config["tag_#{type}_layout"]
+		  binding.pry
 		  data = { 'layout' => layout, 'posts' => posts.sort.reverse!, 'tag' => tag }
           data.merge!(site.config["tag_#{type}_data"] || {})
 
